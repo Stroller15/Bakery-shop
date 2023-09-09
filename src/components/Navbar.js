b30254b431b68b94d1e85195302457ebed8876a7
Your Name <shubhamverma6351@gmail.com>
2023-09-09 21:48:32 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { ShoppingBasket } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Navbar = () => {
+  const { cart } = useSelector((state) => state);
+  return (
+    <>
+      <nav className="flex items-center justify-between h-20  max-w-6xl mx-auto">
+        <Link to={"/"}>
+          <div className="ml-5">
+            <h1 className="bg-gradient-to-br from-violet-900 to-purple-300 bg-clip-text text-transparent text-xl sm:text-2xl md:text-3xl font-bold logo cursor-pointer tracking-wider">
+              Bakery-Shop
+            </h1>
+          </div>
+        </Link>
+        <div className="flex list-none items-center space-x-6 mr-5 text-gray-700 -tracking-tighterr font-semibold">
+          <Link to={"/"}>
+            <li className="cursor-pointer hover:text-purple-500 transition duration-300 ease-in">
+              Home
+            </li>
+          </Link>
+          <Link to={"/cart"}>
+            <div className="relative">
+              <ShoppingBasket className="text-2xl cursor-pointer hover:text-purple-600 transition transform duration-200" />
+
+              {cart.length > 0 && (
+                <div className="absolute bg-purple-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce -top-1 -right-2 rounded-full top- text-white">
+                  {cart.length}
+                </div>
+              )}
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const products = [
+  {
+    id: 1,
+    title: "Lemon cheesecake",
+    price: 120,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/lemoncheese_cake.jpg",
+  },
+  {
+    id: 2,
+    title: "Victoria sponge",
+    price: 140,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/victoria_sponge.jpg",
+  },
+  {
+    id: 3,
+    title: "Carrot cake",
+    price: 160,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/carrot_cake.jpg",
+  },
+  {
+    id: 4,
+    title: "Banana cake",
+    price: 200,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/banana_cake.jpg",
+  },
+  {
+    id: 5,
+    title: "Birthday cake",
+    price: 250,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/birthday_cake.JPG",
+  },
+  {
+    id: 6,
+    title: "Avocado cake",
+    price: 300,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/avocado_cake.jpg",
+  },
+  {
+    id: 7,
+    title: "Chocolate cake",
+    price: 350,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/chocolate_cake.jpg",
+  },
+  {
+    id: 8,
+    title: "Clementine cake",
+    price: 400,
+    image:
+      "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/clementine_cake.png",
+  },
+];
